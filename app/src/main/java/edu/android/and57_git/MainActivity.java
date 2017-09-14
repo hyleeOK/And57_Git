a4384ad0f45dada4dee7a1ec979990e2ecc1f820
hyleeOK <hylee8253@gmail.com>
2017-09-14 11:19:17 +0900

    개발자 aaa 가 수정함

--- app/src/main/java/edu/android/and57_git/MainActivity.java
+++ app/src/main/java/edu/android/and57_git/MainActivity.java
@@ -2,13 +2,19 @@ package edu.android.and57_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+
     }
 
     public void test() {
@@ -22,5 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void testBBB(){
         // 개발자 bbb
+        // 테스트
     }
 }
