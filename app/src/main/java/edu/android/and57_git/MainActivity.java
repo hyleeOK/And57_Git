133b78f05ec6690c236640589eaa7ccae7531374
hyleeOK <hylee8253@gmail.com>
2017-09-14 11:48:20 +0900

    mainActivity 수정 2 aaa

--- app/src/main/java/edu/android/and57_git/MainActivity.java
+++ app/src/main/java/edu/android/and57_git/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView) findViewById(R.id.testView);
+        TextView tv = (TextView) findViewById(R.id.textView);
 
     }
 
